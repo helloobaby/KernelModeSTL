5a2e466d8211ca8cc280b932d10c2fafd4699a8e
sun888486@qq.com <sun888486@qq.com>
2021-09-06 13:04:31 +0800

    update tyoe_traits

--- KernelModeSTL/type_traits.hpp
+++ KernelModeSTL/type_traits.hpp
@@ -6,7 +6,7 @@ Copyright (c) 2021-x	 https://github.com/helloobaby/KernelModeSTL
 /*
 
 暂时考虑user mode msvc的trpe_traits std::is_pod<T>
-因为依赖于编译器的事情，在用户层和内核层并没有什么区别
+因为依赖于编译器并且是编译期的事情，在用户层和内核层并没有什么区别
 
 如果内核中使用出现问题，那么采用本文件中的type_traits
 
